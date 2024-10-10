8119767c072568d86506bcd5eeac36292bac37b6
jhclass03 <jhclass03@naver.com>
2024-10-10 14:08:39 +0900

    scss기본설정완료

---
+++ src/contents/indexData.js
@@ -0,0 +1,45 @@
+
+
+export contst headerNav=[
+  { title:"intro", url:"#intro" },
+  { title:"ability", url:"#ability" },
+  { title:"site", url:"#site" },
+  { title:"project", url:"#project" },
+  { title:"contact", url:"#contact" },
+]
+
+export const introText={
+  title:"frontend developer",
+  desc:["talent","found","effort"]
+}
+
+export const aboutText=[
+  {title:"~", desc:"~~"},
+  {title:"~", desc:"~~"},
+  {title:"~", desc:"~~"},
+]
+
+export const siteText=[
+  {text:[], title:"~",code:"~", view:"~", info:["~","~"]},
+  {text:[], title:"~",code:"~", view:"~", info:["~","~"]},
+  {text:[], title:"~",code:"~", view:"~", info:["~","~"]},
+  {text:[], title:"~",code:"~", view:"~", info:["~","~"]},
+]
+
+export const projectText=[
+  {num:"01", title:"~", desc:"~~", img:~, code:"~", view:"~", name:"~~"},
+  {num:"02", title:"~", desc:"~~", img:~, code:"~", view:"~", name:"~~"},
+  {num:"03", title:"~", desc:"~~", img:~, code:"~", view:"~", name:"~~"},
+  {num:"04", title:"~", desc:"~~", img:~, code:"~", view:"~", name:"~~"},
+  {num:"05", title:"~", desc:"~~", img:~, code:"~", view:"~", name:"~~"},
+  {num:"06", title:"~", desc:"~~", img:~, code:"~", view:"~", name:"~~"},
+  {num:"07", title:"~", desc:"~~", img:~, code:"~", view:"~", name:"~~"},
+  {num:"08", title:"~", desc:"~~", img:~, code:"~", view:"~", name:"~~"},
+  {num:"09", title:"~", desc:"~~", img:~, code:"~", view:"~", name:"~~"},
+  {num:"10", title:"~", desc:"~~", img:~, code:"~", view:"~", name:"~~"},
+]
+
+export const contactText=[
+  {link:"~", title:"~~"},
+  {link:"~", title:"~~"},
+]
